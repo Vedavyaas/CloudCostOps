8b253441e7e1029301f3a17f858ccf8de4d391ce
Vedavyaas M R <vedavyaasmr@Vedavyaass-MacBook-Air.local>
2026-06-15 09:23:17 +0530

    Elastic IP commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const target = process.env.BACKEND_URL || 'http://13.204.66.166:6000';
+const target = process.env.BACKEND_URL || 'http://3.6.254.66:6000';
 
 export default defineConfig({
   plugins: [react()],
